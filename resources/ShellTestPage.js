13b8e2962f98b872bb768aaa037065c367da622c
bwithbala <balakrishnan.tharumar@gmail.com>
2014-06-02 17:08:48 -0400

    Commit

--- resources/ShellTestPage.js
+++ resources/ShellTestPage.js
@@ -5,8 +5,9 @@ try{
 	throw(e);
 }
 
-window.sLorem = "Lorem ipsum dolor sit amet, consetetur "+
-"sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et "+
+window.sLorem = "Lorem ipsum dolor sit amet, consetetur "
+	+
+/*"sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et "+
 "dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam "+
 "et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea "+
 "takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit "+
@@ -163,7 +164,7 @@ window.sLorem = "Lorem ipsum dolor sit amet, consetetur "+
 "lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum "+
 "iriure dolor in hendrerit in vulputate velit esse molestie consequat, "+
 "vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan "+
-"et iusto odio dignissim qui blandit praesent luptatum zzril delenit "+
+"et iusto odio dignissim qui blandit praesent luptatum zzril delenit "+*/
 "augue duis dolore te feugait nulla facilisi.\n\n";
 
 sap.ui.core.Control.extend("SearchFieldPlaceHolder", {
